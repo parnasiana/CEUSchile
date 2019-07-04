caa717ce7c92b95ee6ff1c98d5a441ae9de13238
parnasiana <parnasiana@ug.uchile.cl>
2019-07-04 15:18:20 -0400

    letb.js, footer.html
    slider check function

--- ceus_cl/static/js/letb.js
+++ ceus_cl/static/js/letb.js
@@ -4,4 +4,20 @@ $('#hero-arrow-r').click(function() {
 
     currentSlide.fadeOut(300).removeClass('active');
     nextSlide.fadeIn(300).addClass('active');
+
+    if(nextSlide.length == 0) {
+        $('.slide').first().fadeIn(300).addClass('active');
+    }
 });
+
+$('#hero-arrow-l').click(function() {
+    var currentSlide = $('.slide.active');
+    var prevSlide = currentSlide.prev();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    prevSlide.fadeIn(300).addClass('active');
+
+    if(prevSlide.length == 0) {
+        $('.slide').last().fadeIn(300).addClass('active');
+    }
+})
