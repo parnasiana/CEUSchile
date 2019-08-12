595e21c3c93d13060d6efdde2b94305584cff47f
parnasiana <parnasiana@ug.uchile.cl>
2019-08-12 19:50:07 -0400

    html(subscription, footer, index) css (letb-initial) js (letb)
    Instafeed ceus ok

--- ceus_cl/public/js/letb.js
+++ ceus_cl/public/js/letb.js
@@ -42,10 +42,36 @@ $('.owl-carousel').owlCarousel({
     
 });
 
-twttr.widgets.createTimeline(
-    {
-      sourceType: "profile",
-      screenName: "TwitterDev"
-    },
-    document.getElementById("container")
-  );
+  /*--INSTAFEED--*/
+
+  function buildPost(data){
+    let $wrapper=$('<a>',{class:'post-insta', href: data.link, target:"_blank"});
+    let $img=$('<img>', {src: data.images.low_resolution.url});
+    $wrapper.append($img);
+     $('.instafeed').append($wrapper);
+ }
+
+ function buildNext(url){
+     $('.next-button').remove();
+     let $button =$('<button>', {onclick:'buildPosts("'+url+'")', class:'next-button', text: 'Muéstrame más'});
+         $('.instafeed').append($button);
+ }
+
+ function buildPosts(url){
+     fetch(url)
+         .then(function(response){
+             return response.json()
+         })
+         .then(function(json){
+             for (let data of json.data){
+                 buildPost(data);
+             }
+         buildNext(json.pagination.next_url);            
+
+         })
+ }
+
+ $(document).ready(function(){
+     const url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=4029417903.1677ed0.5d48eeac74c5491e8ee042a507136c9d";
+     buildPosts(url);
+ });
