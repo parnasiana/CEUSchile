b2cfc9d2952d17d263645a70c1a10b2aef3379ef
parnasiana <parnasiana@ug.uchile.cl>
2019-08-26 17:33:46 -0400

    html(navigation, index, aboutus) js(letb)
    nav problem  a href solved by hand

--- ceus_cl/public/js/letb.js
+++ ceus_cl/public/js/letb.js
@@ -1,46 +1,4 @@
-$('#hero-arrow-r').click(function() {
-    var currentSlide = $('.slide.active');
-    var nextSlide = currentSlide.next();
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
-
-    if(nextSlide.length == 0) {
-        $('.slide').first().fadeIn(300).addClass('active');
-    }
-});
-
-$('#hero-arrow-l').click(function() {
-    var currentSlide = $('.slide.active');
-    var prevSlide = currentSlide.prev();
-
-    currentSlide.fadeOut(300).removeClass('active');
-    prevSlide.fadeIn(300).addClass('active');
-
-    if(prevSlide.length == 0) {
-        $('.slide').last().fadeIn(300).addClass('active');
-    }
-});
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:50,
-    nav:true,
-    navText: ["","<i class='fa fa-angle-right arrow-owl-ceus'></i>"],
-    dots: true,
-    responsive:{
-        0: {
-            items:1
-        },
-        600: {
-            items:1
-        },
-        768:{
-            items:3
-        }
-    }
-    
-});
 
   /*--INSTAFEED--*/
 
@@ -72,9 +30,51 @@ $('.owl-carousel').owlCarousel({
  }
 
  $(document).ready(function(){
+    $('#hero-arrow-r').click(function() {
+        var currentSlide = $('.slide.active');
+        var nextSlide = currentSlide.next();
+    
+        currentSlide.fadeOut(300).removeClass('active');
+        nextSlide.fadeIn(300).addClass('active');
+    
+        if(nextSlide.length == 0) {
+            $('.slide').first().fadeIn(300).addClass('active');
+        }
+    });
+    
+    $('#hero-arrow-l').click(function() {
+        var currentSlide = $('.slide.active');
+        var prevSlide = currentSlide.prev();
+    
+        currentSlide.fadeOut(300).removeClass('active');
+        prevSlide.fadeIn(300).addClass('active');
+    
+        if(prevSlide.length == 0) {
+            $('.slide').last().fadeIn(300).addClass('active');
+        }
+    });
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:50,
+        nav:true,
+        navText: ["","<i class='fa fa-angle-right arrow-owl-ceus'></i>"],
+        dots: true,
+        responsive:{
+            0: {
+                items:1
+            },
+            600: {
+                items:1
+            },
+            768:{
+                items:3
+            }
+        }
+        
+    });
      const url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=4029417903.1677ed0.5d48eeac74c5491e8ee042a507136c9d&count=9";
      buildPosts(url);
- });
 
 /*--POP-UP AXES--*/
 
@@ -115,5 +115,11 @@ $(".dev-close, .dev-over").on("click", function() {
 $(".dev-over, .dev-cont").removeClass("appear");
 });
 
-/*--team slider--*/
+$(".no-event").unbind('click');
+
+ });
+
+
+
+
 
